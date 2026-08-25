fc07ec87a3949ccefbad3ca6709bb3f85131924b
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2026-08-25 11:27:57 -0700

    Update update-ov-op-support-list.sh

--- llamacpp/llamacpp-ov-op-support/update-ov-op-support-list.sh
+++ llamacpp/llamacpp-ov-op-support/update-ov-op-support-list.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+: <<'USAGE'
+wget https://raw.githubusercontent.com/ravi9/misc-scripts/refs/heads/main/llamacpp/llamacpp-ov-op-support/update-ov-op-support-list.sh
+bash update-ov-op-support-list.sh
+USAGE
+
 set -euo pipefail
 
-git clone -b ov-op-support-list https://github.com/ravi9/llama.cpp.git
-cd llama.cpp
+git clone -b ov-op-support-list https://github.com/ravi9/llama.cpp.git llama.cpp-ov-op-supp
+cd llama.cpp-ov-op-supp
 
 git checkout ov-op-support-list
 git reset --hard origin/dev_backend_openvino
 git push origin ov-op-support-list --force-with-lease
 
+set +u
 source /opt/intel/openvino/setupvars.sh
+set -u
+
 cmake -B build/ReleaseOV -G Ninja -DCMAKE_BUILD_TYPE=Release -DGGML_OPENVINO=ON
 cmake --build build/ReleaseOV --parallel
 
@@ -19,5 +27,6 @@ GGML_OPENVINO_DEVICE=NPU ./build/ReleaseOV/bin/test-backend-ops support --output
 
 ./scripts/create_ops_docs.py
 
-git add . && git commit -m "Update OV ops docs"
+git add .
+git commit -m "Update OV ops docs"
 git push origin ov-op-support-list
